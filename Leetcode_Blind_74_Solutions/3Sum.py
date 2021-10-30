4649a78424cf07bd9c2b1382083b07c5c721bab0
Varun Shrivastava <75975268+Varun270@users.noreply.github.com>
2021-10-30 19:38:08 +0530

    Add files via upload

---
+++ Leetcode_Blind_74_Solutions/3Sum.py
@@ -0,0 +1,27 @@
+# Problem Link --> https://leetcode.com/problems/3sum/
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        nums.sort()
+        
+        result = []
+        for i in range(len(nums)-2):
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+            
+            l, r = i + 1, len(nums) - 1
+            while l < r: 
+                sum_ = nums[i] + nums[l] + nums[r]                
+                if sum_ < 0:
+                    l += 1
+                elif sum_ > 0:
+                    r -= 1
+                else:
+                    result.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    l += 1                        
+                    r -= 1
+        
+        return result
